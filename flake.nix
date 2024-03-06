f40caa67b8841af8a4e31426435c6dd907f2d5ae
eureka-cpu <github.eureka@gmail.com>
2024-03-06 00:04:38 -0800

    try importing default file

--- flake.nix
+++ flake.nix
@@ -2,8 +2,6 @@
   description = "Declarative Helix editor themes for home-manager";
 
   outputs = { self }: {
-    themes = {
-      inherit (./themes/default.nix) kanabox_default gruvbox_material_dark_medium;
-    };
+    themes = import ./themes/default.nix;
   };
 }
