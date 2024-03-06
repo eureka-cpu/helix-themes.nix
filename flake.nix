db80d1131d3ca99a2ed2602abfba1f8105e8478c
eureka-cpu <github.eureka@gmail.com>
2024-03-05 23:17:28 -0800

    rm braces

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
 
   outputs = { self }: {
     themes = {
-      kanabox_default = (import ./themes/kanabox_default.nix) {};
-      gruvbox_material_dark_medium = (import ./themes/gruvbox_material_dark_medium.nix) {}; 
+      kanabox_default = (import ./themes/kanabox_default.nix);
+      gruvbox_material_dark_medium = (import ./themes/gruvbox_material_dark_medium.nix); 
     };
   };
 }
